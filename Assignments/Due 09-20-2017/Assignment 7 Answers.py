7c6177b56a8b33bb101315e910f932981adb58a1
Matthew Harris <matthew@biofare.org>
2017-09-17 19:34:12 -0700

    start on homework, figured out if its a perfect square. need to address the sum.

---
+++ Assignments/Due 09-20-2017/Assignment 7 Answers.py
@@ -0,0 +1,25 @@
+import math
+
+def squareChecker(number):
+	status = False
+
+	simplifiedN = math.sqrt(number)
+
+	if (number % simplifiedN) == 0 :
+		print("\nIt's a perfect square\n")
+
+	else:
+		print("\nIt's not a perfect square\n")
+
+
+def main():
+	while True:
+		number = raw_input("Enter a number: ")
+		try:
+			number = int(number)
+			break
+		except ValueError:
+			print("\nThat's not a valid option!  Enter a number!\n")
+	squareChecker(number)
+
+main()
